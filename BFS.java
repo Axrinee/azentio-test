63512b0af3786850daa592fce72075b39c19dd38
Axrinee <harinishree085@gmail.com>
2025-04-05 16:41:31 +0530

    Create BFS.java

---
+++ BFS.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class GraphBFS {
+
+    private int vertices;
+    private LinkedList<Integer>[] adjacencyList;
+
+    public GraphBFS(int v) {
+        vertices = v;
+        adjacencyList = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            adjacencyList[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int src, int dest) {
+        adjacencyList[src].add(dest);
+        
+    }
+
+    public void bfsTraversal(int startVertex) {
+        boolean[] visited = new boolean[vertices];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[startVertex] = true;
+        queue.add(startVertex);
+
+        System.out.print("BFS Traversal: ");
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current + " ");
+
+            for (int neighbor : adjacencyList[current]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        GraphBFS graph = new GraphBFS(6);
+
+        graph.addEdge(0, 1);
+        graph.addEdge(0, 2);
+        graph.addEdge(1, 3);
+        graph.addEdge(1, 4);
+        graph.addEdge(2, 5);
+
+        graph.bfsTraversal(0);
+    }
+}
